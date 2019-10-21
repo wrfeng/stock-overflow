6a4750dc9211e9bb24e99776c5ae3e1d145c9644
Wei <theweifeng@gmail.com>
2019-10-21 16:04:48 -0400

    fix deploy bug

--- db/migrate/20191003200607_change_user_column.rb
+++ db/migrate/20191003200607_change_user_column.rb
@@ -1,6 +1,5 @@
 class ChangeUserColumn < ActiveRecord::Migration[5.2]
   def change
   end
-  add_column :users, :account_balance, :decimal, precision: 10, scale: 2, default: 5000
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_175154) do
+ActiveRecord::Schema.define(version: 2019_10_04_020839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2019_10_04_175154) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "price_total", precision: 10, scale: 2
-    t.string "action"
   end
 
   create_table "users", force: :cascade do |t|
